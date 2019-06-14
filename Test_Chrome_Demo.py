97d5e4b52e568c812cd36980c1ace29680c9ba9d
Alex <349786528@qq.com>
2019-06-14 16:22:13 +0800

    Test

---
+++ Test_Chrome_Demo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("http://baidu.com")
+
+driver.find_element_by_id("kw").send_keys("selenium")
+driver.find_element_by_id("su").click()
+driver.quit()
+
+
+
+
